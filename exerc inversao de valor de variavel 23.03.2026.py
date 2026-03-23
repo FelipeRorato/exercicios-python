36144a7d724bb92cd9be8a279349a058a4ce468a
FelipeRorato <feliperoratocosta1@gmail.com>
2026-03-23 13:30:58 -0300

    Delete exerc inversao de valor de variavel 23.03.2026.py

--- exerc inversao de valor de variavel 23.03.2026.py
+++
@@ -1,9 +0,0 @@
-A = float(input("Insira o valor de A:"))
-B = float(input("Insira o valor de B:"))
-C = float(input("Insira o valor de C:"))
-
-A, B, C = C, B, A
-
-print("A =", A)
-print("B =", B)
-print("C =", C)
