8dc7f535cafb98cd9be2207b46cc941630c1043d
FelipeRorato <feliperoratocosta1@gmail.com>
2026-03-23 13:30:42 -0300

    Add files via upload

---
+++ exerc inversao de valor de variavel 23.03.2026.py
@@ -0,0 +1,9 @@
+A = float(input("Insira o valor de A:"))
+B = float(input("Insira o valor de B:"))
+C = float(input("Insira o valor de C:"))
+
+A, B, C = C, B, A
+
+print("A =", A)
+print("B =", B)
+print("C =", C)
