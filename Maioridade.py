389bf843e7666718eb1667f64e1f8281d774f08c
FelipeRorato <feliperoratocosta1@gmail.com>
2026-03-31 09:08:52 -0300

    Add files via upload

---
+++ Maioridade.py
@@ -0,0 +1,10 @@
+titulo = 'maioridade'
+print(f'{titulo:^30}')
+idade = int(input('Que idade você tem?'))
+
+if idade >= 18:
+    print("Você é de maior")
+
+else:
+    print('Você é de menor')
+print('obrigado por participar')
